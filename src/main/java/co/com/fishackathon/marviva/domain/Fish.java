2d42626b8a6130ed4220fa026f8e010a410e5ca5
Tachú <tachu.salamanca@gmail.com>
2016-04-23 00:21:02 -0500

    entidad fish v1

--- src/main/java/co/com/fishackathon/marviva/domain/Fish.java
+++ src/main/java/co/com/fishackathon/marviva/domain/Fish.java
@@ -1,8 +1,8 @@
 package co.com.fishackathon.marviva.domain;
 
-
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -19,67 +19,47 @@ public class Fish implements Serializable {
     private Long id;
 
     @Column(name = "nombre_comun")
-    private String nombre_comun;
+    private String nombreComun;
 
     @Column(name = "nombre_cientifico")
-    private String nombre_cientifico;
+    private String nombreCientifico;
+    
+    @Column(name = "nombre_etiquetado")
+    private String nombreEtiquetado;
 
-    @Column(name = "longitud")
-    private Long longitud;
+    @Column(name = "otros_nombres")
+    @ElementCollection
+    private List<String> otrosNombres;
+    
+    @Column(name = "longitud_minima")
+    private Long longitudMinima;
 
-    @Column(name = "peso")
-    private Long peso;
+    @Column(name = "longitud_maxima")
+    private Long longitudMaxima;
+
+    @Column(name = "peso_minimo")
+    private Long pesoMinimo;
+
+    @Column(name = "peso_maximo")
+    private Long pesoMaximo;
 
     @Column(name = "imagen")
     private String imagen;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "origen")
+    private String origen;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(name = "coloracion")
+    private String coloracion;
 
-    public String getNombre_comun() {
-        return nombre_comun;
-    }
+    @Column(name = "olor")
+    private String olor;
 
-    public void setNombre_comun(String nombre_comun) {
-        this.nombre_comun = nombre_comun;
-    }
-
-    public String getNombre_cientifico() {
-        return nombre_cientifico;
-    }
-
-    public void setNombre_cientifico(String nombre_cientifico) {
-        this.nombre_cientifico = nombre_cientifico;
-    }
-
-    public Long getLongitud() {
-        return longitud;
-    }
-
-    public void setLongitud(Long longitud) {
-        this.longitud = longitud;
-    }
-
-    public Long getPeso() {
-        return peso;
-    }
-
-    public void setPeso(Long peso) {
-        this.peso = peso;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
+    @Column(name = "precio_kg")
+    private String precioKg;
+    
+    @Column(name = "estado")
+    private String estado;
 
     @Override
     public boolean equals(Object o) {
@@ -90,26 +70,224 @@ public class Fish implements Serializable {
             return false;
         }
         Fish fish = (Fish) o;
-        if(fish.id == null || id == null) {
+        if(fish.getId() == null || getId() == null) {
             return false;
         }
-        return Objects.equals(id, fish.id);
+        return Objects.equals(getId(), fish.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        return Objects.hashCode(getId());
     }
 
-    @Override
-    public String toString() {
-        return "Fish{" +
-            "id=" + id +
-            ", nombre_comun='" + nombre_comun + "'" +
-            ", nombre_cientifico='" + nombre_cientifico + "'" +
-            ", longitud='" + longitud + "'" +
-            ", peso='" + peso + "'" +
-            ", imagen='" + imagen + "'" +
-            '}';
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombreComun
+     */
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    /**
+     * @param nombreComun the nombreComun to set
+     */
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    /**
+     * @return the nombreCientifico
+     */
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    /**
+     * @param nombreCientifico the nombreCientifico to set
+     */
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    /**
+     * @return the nombreEtiquetado
+     */
+    public String getNombreEtiquetado() {
+        return nombreEtiquetado;
+    }
+
+    /**
+     * @param nombreEtiquetado the nombreEtiquetado to set
+     */
+    public void setNombreEtiquetado(String nombreEtiquetado) {
+        this.nombreEtiquetado = nombreEtiquetado;
+    }
+
+    /**
+     * @return the otrosNombres
+     */
+    public List<String> getOtrosNombres() {
+        return otrosNombres;
+    }
+
+    /**
+     * @param otrosNombres the otrosNombres to set
+     */
+    public void setOtrosNombres(List<String> otrosNombres) {
+        this.otrosNombres = otrosNombres;
+    }
+
+    /**
+     * @return the longitudMinima
+     */
+    public Long getLongitudMinima() {
+        return longitudMinima;
+    }
+
+    /**
+     * @param longitudMinima the longitudMinima to set
+     */
+    public void setLongitudMinima(Long longitudMinima) {
+        this.longitudMinima = longitudMinima;
+    }
+
+    /**
+     * @return the longitudMaxima
+     */
+    public Long getLongitudMaxima() {
+        return longitudMaxima;
+    }
+
+    /**
+     * @param longitudMaxima the longitudMaxima to set
+     */
+    public void setLongitudMaxima(Long longitudMaxima) {
+        this.longitudMaxima = longitudMaxima;
+    }
+
+    /**
+     * @return the pesoMinimo
+     */
+    public Long getPesoMinimo() {
+        return pesoMinimo;
+    }
+
+    /**
+     * @param pesoMinimo the pesoMinimo to set
+     */
+    public void setPesoMinimo(Long pesoMinimo) {
+        this.pesoMinimo = pesoMinimo;
+    }
+
+    /**
+     * @return the pesoMaximo
+     */
+    public Long getPesoMaximo() {
+        return pesoMaximo;
+    }
+
+    /**
+     * @param pesoMaximo the pesoMaximo to set
+     */
+    public void setPesoMaximo(Long pesoMaximo) {
+        this.pesoMaximo = pesoMaximo;
+    }
+
+    /**
+     * @return the imagen
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * @param imagen the imagen to set
+     */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    /**
+     * @return the origen
+     */
+    public String getOrigen() {
+        return origen;
+    }
+
+    /**
+     * @param origen the origen to set
+     */
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    /**
+     * @return the coloracion
+     */
+    public String getColoracion() {
+        return coloracion;
+    }
+
+    /**
+     * @param coloracion the coloracion to set
+     */
+    public void setColoracion(String coloracion) {
+        this.coloracion = coloracion;
+    }
+
+    /**
+     * @return the olor
+     */
+    public String getOlor() {
+        return olor;
+    }
+
+    /**
+     * @param olor the olor to set
+     */
+    public void setOlor(String olor) {
+        this.olor = olor;
+    }
+
+    /**
+     * @return the precioKg
+     */
+    public String getPrecioKg() {
+        return precioKg;
+    }
+
+    /**
+     * @param precioKg the precioKg to set
+     */
+    public void setPrecioKg(String precioKg) {
+        this.precioKg = precioKg;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
