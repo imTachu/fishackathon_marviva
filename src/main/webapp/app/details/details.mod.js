6c04e5bd4fce760593cc5fb6445337144fcfcb7f
Andrés Esguerra <afesguerra@outlook.com>
2016-04-23 22:08:36 -0500

    clases estados de conservacion

--- src/main/webapp/app/details/details.mod.js
+++ src/main/webapp/app/details/details.mod.js
@@ -4,10 +4,34 @@ angular.module('marvivaApp')
                     url: '/details/{id:int}',
                     templateUrl: 'app/details/details.html',
                     controller: 'detailsCtrl',
+                    abstract: true,
                     resolve: {
-                        fish: ['Restangular', '$stateParams', function(r, params){
+                        fish: ['Restangular', '$stateParams', function (r, params) {
+                                return {
+                                    id: 1,
+                                    nombreComun: 'Pargo',
+                                    nombreCientifico: 'Iltonus arrogantus'
+                                };
                                 return r.one('fish', params.id).get();
-                        }]
+                            }]
                     }
+                }).state('details.consumer', {
+                    url: '/consumer',
+                    templateUrl: 'app/details/consumer.html'
+                }).state('details.bio', {
+                    url: '/bio',
+                    templateUrl: 'app/details/consumer.html'
+                }).state('details.commercial', {
+                    url: '/commercial',
+                    templateUrl: 'app/details/consumer.html'
+                }).state('details.nutrition', {
+                    url: '/nutrition',
+                    templateUrl: 'app/details/consumer.html'
+                }).state('details.economy', {
+                    url: '/economy',
+                    templateUrl: 'app/details/consumer.html'
+                }).state('details.regulation', {
+                    url: '/regulation',
+                    templateUrl: 'app/details/consumer.html'
                 });
             }]);
--- src/main/webapp/app/search/search.mod.js
+++ src/main/webapp/app/search/search.mod.js
@@ -5,13 +5,30 @@ angular.module('marvivaApp')
                     templateUrl: 'app/search/search.html',
                     controller: 'searchCtrl',
                     resolve: {
-                        fishes: ['Restangular', '$stateParams', function(r, params){
+                        fishes: ['Restangular', '$stateParams', function (r, params) {
+                                return [{
+                                        id: 1,
+                                        nombreComun: 'Pargo',
+                                        nombreCientifico: 'Iltonus arrogantus'
+                                    }, {
+                                        id: 2,
+                                        nombreComun: 'Oso',
+                                        nombreCientifico: 'Fish Fisher'
+                                    }, {
+                                        id: 2,
+                                        nombreComun: 'Oso',
+                                        nombreCientifico: 'Fish Fisher'
+                                    }, {
+                                        id: 2,
+                                        nombreComun: 'Oso',
+                                        nombreCientifico: 'Fish Fisher'
+                                    }]
                                 var builder = r.all('fish');
-                                if(params.q){
+                                if (params.q) {
                                     return builder.getList({q: params.q});
                                 }
                                 return builder.getList();
-                        }]
+                            }]
                     }
                 });
             }]);
