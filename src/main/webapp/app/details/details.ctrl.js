f15332d761c4dc5ea1a43e61e7ed9ca270a5ef5b
Andrés Esguerra <afesguerra@outlook.com>
2016-04-24 00:40:33 -0500

    conservacion

--- src/main/webapp/app/details/details.ctrl.js
+++ src/main/webapp/app/details/details.ctrl.js
@@ -1,6 +1,7 @@
 angular.module('marvivaApp')
         .controller('detailsCtrl', ['$scope', 'fish', '$state', function($scope, fish, $state){
                 $scope.fish = fish;
+                $scope.conservationClass = fish.estadoConservacion==='SIN_RIESGO'?'na':fish.estadoConservacion.toLowerCase();
                 $scope.search = function(q){
                     $state.go('search', {q: q});
                 };
