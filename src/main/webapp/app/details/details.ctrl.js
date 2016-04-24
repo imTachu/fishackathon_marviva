fbe75622aea11a9ff0e48a6847ab0e1473576781
Tachú <tachu.salamanca@gmail.com>
2016-04-24 10:14:02 -0500

    collapse

--- src/main/webapp/app/details/details.ctrl.js
+++ src/main/webapp/app/details/details.ctrl.js
@@ -1,11 +1,16 @@
 angular.module('marvivaApp')
-        .controller('detailsCtrl', ['$scope', 'fish', '$state', function ($scope, fish, $state) {
-                $scope.fish = fish;
-                $scope.conservationClass = fish.estadoConservacion === 'SIN_RIESGO' ? 'na' : fish.estadoConservacion.toLowerCase();
-                $scope.search = function (q) {
-                    $state.go('search', {q: q});
-                };
-                $scope.setMain = function(i){
-                    $scope.mainImage = i;
-                }
-            }]);
+.controller('detailsCtrl', ['$scope', 'fish', '$state', function ($scope, fish, $state) {
+    $scope.fish = fish;
+    $scope.conservationClass = fish.estadoConservacion === 'SIN_RIESGO' ? 'na' : fish.estadoConservacion.toLowerCase();
+    $scope.search = function (q) {
+        $state.go('search', {q: q});
+    };
+    $scope.setMain = function(i){
+        $scope.mainImage = i;
+    };
+    $(document).on('click','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') ) {
+            $(this).collapse('hide');
+        }
+    });
+}]);
