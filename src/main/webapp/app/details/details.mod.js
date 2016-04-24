3beb224b058d0b3831868470f6c3dcb0c0f74ce2
Andrés Esguerra <afesguerra@outlook.com>
2016-04-24 00:33:06 -0500

    detalles

--- src/main/webapp/app/details/details.mod.js
+++ src/main/webapp/app/details/details.mod.js
@@ -15,18 +15,18 @@ angular.module('marvivaApp')
                     templateUrl: 'app/details/consumer.html'
                 }).state('details.bio', {
                     url: '/bio',
-                    templateUrl: 'app/details/consumer.html'
+                    templateUrl: 'app/details/bio.html'
                 }).state('details.commercial', {
                     url: '/commercial',
-                    templateUrl: 'app/details/consumer.html'
+                    templateUrl: 'app/details/commercial.html'
                 }).state('details.nutrition', {
                     url: '/nutrition',
-                    templateUrl: 'app/details/consumer.html'
+                    templateUrl: 'app/details/nutrition.html'
                 }).state('details.economy', {
                     url: '/economy',
-                    templateUrl: 'app/details/consumer.html'
+                    templateUrl: 'app/details/economy.html'
                 }).state('details.regulation', {
                     url: '/regulation',
-                    templateUrl: 'app/details/consumer.html'
+                    templateUrl: 'app/details/regulation.html'
                 });
             }]);
